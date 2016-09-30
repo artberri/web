457b04402d79871c8f96973fefe0bcf2d570c48c
Alberto Varela <alberto@berriart.com>
2016-09-30 20:02:21 +0200

    Adding letsencrypt cron and updateing deps

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,5 @@
 (function () {
 
-    var $menuIcon =  $('#menu-icon');
-
-    $('#menu-button').click(function (e) {
-        e.preventDefault();
-
-        $('body').toggleClass('menu-opened');
-        $menuIcon.toggleClass('fi-list fi-x');
-    });
-
-    $('.home .main-menu .menu-item-object-custom a').click(function (e) {
-        $('body').toggleClass('menu-opened');
-        $menuIcon.toggleClass('fi-list fi-x');
-    });
-
     $('.smooth').click(function (e) {
         e.preventDefault();
 
