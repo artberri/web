dfc88ed8db308177c01d82c3d3b1f6459d533c3c
Alberto Varela <alberto@berriart.com>
2021-05-25 17:08:28 +0200

    deps: upgrade dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
+const postcssNormalize = require('postcss-normalize');
+
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('postcss-normalize'),
-        require('precss'),
+        postcssNormalize(),
+        require('postcss-mixins'),
+        require('postcss-simple-vars'),
+        require('postcss-nested'),
         require('autoprefixer'),
         require('cssnano')({
             preset: 'default'
