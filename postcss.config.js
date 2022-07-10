b022f98397091a8176441086104349ba273d8a70
Alberto Varela <alberto@berriart.com>
2022-07-11 01:46:25 +0200

    fix classes

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,13 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./hugo_stats.json"],
   defaultExtractor: (content) => {
     const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+    return [
+      "light",
+      "dark",
+      ...(els.tags || []),
+      ...(els.classes || []),
+      ...(els.ids || []),
+    ];
   },
   safelist: [],
 });
