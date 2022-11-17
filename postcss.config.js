8f6979abdb74fd91cacf62bc26c490c33eab30a2
Alberto Varela Sánchez <hello@albertovarela.net>
2022-11-17 23:58:29 +0100

    upgrade packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,43 +1,43 @@
-const isProduction = process.env.HUGO_ENVIRONMENT === "production";
-const postcssImport = require("postcss-import");
-const postcssNormalize = require("postcss-normalize");
-const postcssPresetEnv = require("postcss-preset-env")({
-  features: {
-    "nesting-rules": true,
-  },
-});
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./hugo_stats.json"],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return ["light", "dark", ...(els.tags || []), ...(els.classes || [])];
-  },
-  safelist: { deep: [/chroma/] },
-});
+const isProduction = process.env.HUGO_ENVIRONMENT === 'production'
+const postcssImport = require('postcss-import')
+const postcssNormalize = require('postcss-normalize')
+const postcssPresetEnv = require('postcss-preset-env')({
+	features: {
+		'nesting-rules': true,
+	},
+})
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: ['./hugo_stats.json'],
+	defaultExtractor: (content) => {
+		const els = JSON.parse(content).htmlElements
+		return ['light', 'dark', ...(els.tags || []), ...(els.classes || [])]
+	},
+	safelist: { deep: [/chroma/] },
+})
 
-const fontsPath = isProduction ? "public/fonts" : "static/fonts";
-const postcssUrl = require("postcss-url")([
-  {
-    filter: /\/@fontsource\//,
-    url: "copy",
-    assetsPath: fontsPath,
-    useHash: true,
-  },
-  {
-    filter: /\/@fontsource\//,
-    url: (asset) => {
-      return asset.url.replace(fontsPath, "/fonts");
-    },
-    multi: true,
-  },
-]);
+const fontsPath = isProduction ? 'public/fonts' : 'static/fonts'
+const postcssUrl = require('postcss-url')([
+	{
+		filter: /\/@fontsource\//,
+		url: 'copy',
+		assetsPath: fontsPath,
+		useHash: true,
+	},
+	{
+		filter: /\/@fontsource\//,
+		url: (asset) => {
+			return asset.url.replace(fontsPath, '/fonts')
+		},
+		multi: true,
+	},
+])
 
 module.exports = {
-  plugins: [
-    postcssImport,
-    postcssUrl,
-    postcssNormalize,
-    postcssPresetEnv,
-    ...(isProduction ? [purgecss] : []),
-  ],
-};
+	plugins: [
+		postcssImport,
+		postcssUrl,
+		postcssNormalize,
+		postcssPresetEnv,
+		...(isProduction ? [purgecss] : []),
+	],
+}
--- static/js/sidr.js
+++ static/js/sidr.js
@@ -1,76 +1,76 @@
-(function () {
-  "use strict";
+;(function () {
+	'use strict'
 
-  var hasTouchEvents =
-    "ontouchstart" in window ||
-    (window.DocumentTouch && document instanceof DocumentTouch);
+	var hasTouchEvents =
+		'ontouchstart' in window ||
+		(window.DocumentTouch && document instanceof DocumentTouch)
 
-  $("#simple-menu").sidr();
+	$('#simple-menu').sidr()
 
-  $("#left-menu").sidr({
-    name: "sidr-left",
-    side: "left", // By default
-  });
+	$('#left-menu').sidr({
+		name: 'sidr-left',
+		side: 'left', // By default
+	})
 
-  $("#right-menu").sidr({
-    name: "sidr-right",
-    side: "right",
-  });
+	$('#right-menu').sidr({
+		name: 'sidr-right',
+		side: 'right',
+	})
 
-  $("#existing-content-menu").sidr({
-    name: "sidr-existing-content",
-    source: ".documentation-header, .entry-subtitle",
-  });
+	$('#existing-content-menu').sidr({
+		name: 'sidr-existing-content',
+		source: '.documentation-header, .entry-subtitle',
+	})
 
-  if (window.location.host !== "localhost:4000") {
-    $("#remote-content-menu").sidr({
-      name: "sidr-remote-content",
-      source:
-        window.location.protocol +
-        "//" +
-        window.location.host +
-        "/remote-menu.html",
-    });
-  }
+	if (window.location.host !== 'localhost:4000') {
+		$('#remote-content-menu').sidr({
+			name: 'sidr-remote-content',
+			source:
+				window.location.protocol +
+				'//' +
+				window.location.host +
+				'/remote-menu.html',
+		})
+	}
 
-  $("#callback-menu").sidr({
-    name: "sidr-callback",
-    source: function (name) {
-      return (
-        "<h1>" + name + " menu</h1><p>Yes! You can use a callback too ;)</p>"
-      );
-    },
-  });
+	$('#callback-menu').sidr({
+		name: 'sidr-callback',
+		source: function (name) {
+			return (
+				'<h1>' + name + ' menu</h1><p>Yes! You can use a callback too ;)</p>'
+			)
+		},
+	})
 
-  $("#responsive-menu-button").sidr({
-    name: "sidr-main",
-    source: "#header .menu",
-  });
+	$('#responsive-menu-button').sidr({
+		name: 'sidr-main',
+		source: '#header .menu',
+	})
 
-  if (hasTouchEvents) {
-    $("body").swipe({
-      //Single swipe handler for left swipes
-      swipeLeft: function () {
-        $.sidr("close", "sidr-main");
-      },
-      swipeRight: function () {
-        $.sidr("open", "sidr-main");
-      },
-      //Default is 75px, set to 0 for demo so any distance triggers swipe
-      threshold: 45,
-    });
-  }
+	if (hasTouchEvents) {
+		$('body').swipe({
+			//Single swipe handler for left swipes
+			swipeLeft: function () {
+				$.sidr('close', 'sidr-main')
+			},
+			swipeRight: function () {
+				$.sidr('open', 'sidr-main')
+			},
+			//Default is 75px, set to 0 for demo so any distance triggers swipe
+			threshold: 45,
+		})
+	}
 
-  /* Analytics */
-  $("#download").click(function () {
-    if (ga) {
-      ga("send", "event", "Sidr", "Download");
-    }
-  });
+	/* Analytics */
+	$('#download').click(function () {
+		if (ga) {
+			ga('send', 'event', 'Sidr', 'Download')
+		}
+	})
 
-  $(".demo.button").click(function () {
-    if (ga) {
-      ga("send", "event", "Sidr", "Demo", $(this).attr("href"));
-    }
-  });
-})();
+	$('.demo.button').click(function () {
+		if (ga) {
+			ga('send', 'event', 'Sidr', 'Demo', $(this).attr('href'))
+		}
+	})
+})()
